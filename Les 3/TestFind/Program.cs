8c20ba0a03801e31d29bcb7ededa8784e0f9d5b1
Etiënne Goossens <psm.goossens@avans.nl>
2019-09-06 11:01:30 +0200

    Les 3 toegevoegd

---
+++ Les 3/TestFind/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 0, 2, 4, 6, 8, 10, 12, 14, 16 };
+            Predicate<int> groterDan100 = x => x * x > 100;
+
+            int kwadraatGroterDan100 = Array.Find(a, n => n*n > 100);
+            int[] allen = Array.FindAll(a, groterDan100);
+
+            Console.WriteLine($"Eerste waarde die voldoet: {kwadraatGroterDan100}");
+
+            Console.WriteLine("\nAlle waarden die voldoen:");
+            foreach (int x in allen)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
