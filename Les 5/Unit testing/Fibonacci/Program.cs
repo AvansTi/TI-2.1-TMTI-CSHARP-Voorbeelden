120ac6ad07559d55a03d0bc7878b69a0551d6e6a
Etiënne Goossens <psm.goossens@avans.nl>
2019-09-20 12:07:28 +0200

    Les 5 initial commit

---
+++ Les 5/Unit testing/Fibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Calculate
+    {
+
+        public int lastNumber = 0;
+        public int Fibonacci(int n) {
+            if (n >= 47)
+                throw new
+                    OverflowException("Fibonacci with interger datatype is limited to n <= 47");
+
+            if (n < 0)
+                throw new ArgumentException("N >= 0");
+
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++) {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            lastNumber = a;
+            return a;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        
+    }
+}
